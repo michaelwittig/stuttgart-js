cd360d86ff1de3efbb0772667dc472aa7590e09e
Michael Wittig <post@michaelwittig.info>
2012-11-10 10:48:08 +0100

    added method

--- methodhandler.js
+++ methodhandler.js
@@ -1,12 +1,17 @@
 /**
  * Invocations of methods (done by JSON-RPC) are handled here.
  */
-define([], function() {
+define(["common/logger"], function(logger) {
 	"use strict";
 
 	var methods = {
-		test: function(params, callback) {
-			callback(undefined, {});
+		"test": function(param1, param2, callback) {
+			logger.debug("test", {a: param1, b: param2});
+			callback(undefined, {a: 10});
+		},
+		"board:create": function(board, callback) {
+			logger.debug("board:create", board);
+			callback(new Error("NOT IMPLEMENTED"), undefined);
 		}
 	};
 
@@ -20,7 +25,8 @@ define([], function() {
 		handle: function(method, params, callback) {
 			var fn = methods[method];
 			if (typeof fn === "function") {
-				fn(params, callback);
+				params.push(callback);
+				fn.apply(undefined, params);
 			} else {
 				callback(new Error("No JSON-RPC handler found for method " + method));
 			}

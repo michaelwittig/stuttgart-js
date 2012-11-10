cf980cd16f7cba47dd5e3e54298b937a55dfcecc
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-10 11:59:46 +0100

    boilerplates for the models

---
+++ static/js/models/appState.js
@@ -0,0 +1,10 @@
+define(['backbone', function(Backbone) {
+
+  var AppState = Backbone.Model.extend({
+    initialize: function() {
+
+    }
+  });
+
+  return AppState;
+}]);
---
+++ static/js/models/user.js
@@ -0,0 +1,17 @@
+define(['backbone', function(Backbone) {
+
+  var User = Backbone.Model.extend({
+    defaults: {
+      id: 0,
+      name: '',
+      photo: ''
+    },
+
+    initialize: function() {
+
+    }
+
+  });
+
+  return User;
+}]);

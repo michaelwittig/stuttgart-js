87374212b79ba292d333bacc1fb914d94dc96a63
Michael Wittig <post@michaelwittig.info>
2012-11-10 09:38:09 +0100

    ctrl-c to stop. added uncaught exception handler

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ requirejs.config({
 	nodeRequire: require
 });
 
-requirejs(["server", "logger"], function(server, logger) {
+requirejs(["server", "common/logger"], function(server, logger) {
 	"use strict";
+
+	process.on("uncaughtException", function (err) {
+		logger.error("uncaught exception", err);
+		process.exit(1);
+	});
+
+	process.on("SIGINT", function () {
+		logger.notice("Going down...");
+		server.stop(function(err) {
+			if (err) {
+				logger.error("can not stop", err);
+				process.exit(1);
+
+			} else {
+				logger.notice("stopped");
+				process.exit(0);
+			}
+		});
+	});
 
     server.start(function(err) {
         if (err) {
             logger.error("can not start", err);
-            process.exit(0);
+            process.exit(1);
         } else {
             logger.notice("started");
         }

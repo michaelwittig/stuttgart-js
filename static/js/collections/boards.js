54e3a56f90341b102588e043439f2725336c154a
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-10 23:50:24 +0100

    boardList on click open Board

--- static/js/collections/boards.js
+++ static/js/collections/boards.js
@@ -8,41 +8,41 @@ define(
 
         initialize: function() {
 
-	    registry.user.on('change:loc', this.fetch, this);
+            registry.user.on('change:loc', this.fetch, this);
 
-	    registry.state.on('change:facebook', function() {
-		if (registry.state.get('facebook') !== 'NOTREADY') {
-		    this.loadFacebookData();
-		}
-	    }, this);
+            registry.state.on('change:facebook', function() {
+                if (registry.state.get('facebook') !== 'NOTREADY') {
+                    this.loadFacebookData();
+                }
+            }, this);
 
         },
 
-	sync: sync,
+        sync: sync,
 
-	loadFacebookData: function() {
-	    var ids = this.map(function(board,i) {
-		//TODO: this is hardcoded!
-		var id = ['1476961015','504056176','592786071','620243290','648200655','658533506','658948132','718688296','721185145','721290847'][i];
-		board.set('user', {id: id});
-		return id;
-		// return board.get('user')['id'];
-	    });
-	    registry.facebook.fetchUsers(ids, _.bind(function(err, res) {
-		//TODO: handle err
-		if (err) return;
-		_.each(res, function(user) {
-		    var board = this.find(function(board) {
-			return board.get('user')['id'] === user.uid;
-		    });
-		    board.set({
-			photo: user.pic_square,
-			creator: user.name
-		    });
-		}, this);
+        loadFacebookData: function() {
+            var ids = this.map(function(board,i) {
+                //TODO: this is hardcoded!
+                var id = ['1476961015','504056176','592786071','620243290','648200655','658533506','658948132','718688296','721185145','721290847'][i];
+                board.set('user', {id: id});
+                return id;
+                // return board.get('user')['id'];
+            });
+            registry.facebook.fetchUsers(ids, _.bind(function(err, res) {
+                //TODO: handle err
+                if (err) return;
+                _.each(res, function(user) {
+                    var board = this.find(function(board) {
+                        return board.get('user')['id'] === user.uid;
+                    });
+                    board.set({
+                        photo: user.pic_square,
+                        creator: user.name
+                    });
+                }, this);
 
-	    }, this));
-	}
+            }, this));
+        }
     });
 
     return Boards;
--- static/js/views/boardsListView.js
+++ static/js/views/boardsListView.js
@@ -5,17 +5,27 @@ define(['backbone', 'hbs!templates/boardsListTemplate', 'utils/registry'], funct
     el: '#board-list',
 
     initialize: function() {
-	registry.boards.on('change', this.render, this);
-	registry.boards.on('reset', this.render, this);
+        registry.boards.on('change', this.render, this);
+        registry.boards.on('reset', this.render, this);
+        registry.boards.on('facebook:loaded', this.render, this);
     },
 
     render: function() {
-	console.log('board list render',registry.boards.toJSON());
-	this.$el.html(template(registry.boards.toJSON()));
+        console.log('board list render',registry.boards.toJSON());
+        this.$el.html(template(registry.boards.toJSON()));
     },
 
     show: function() {
-	this.$el.show();
+        this.$el.show();
+    },
+
+    events: {
+        'click li': 'openBoard'
+    },
+
+    openBoard: function(e) {
+        var boardId = $(e.currentTarget).attr('data-id');
+        registry.router.navigate('board/' + boardId);
     }
     });
 

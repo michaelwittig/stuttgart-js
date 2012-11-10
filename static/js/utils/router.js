cffe5c9af9ed324f71e1801f4514f09a03d81410
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-10 11:18:25 +0100

    clientside router setup

--- static/js/utils/router.js
+++ static/js/utils/router.js
@@ -1,5 +1,14 @@
-/home
-/home/:lat/:lng
-/boards
-/board/:id
-/new
+define(['utils/registry'], function(registry) {
+
+  var Router = Backbone.Router.extend({
+   routes: {
+      '/home': 'home'
+    },
+
+    home: function() {
+      registry.state.trigger('route:home');
+    }
+  });
+
+  return Router;
+});

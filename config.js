97b7772584779701007d349b3320bd8fa4fd6fff
Michael Wittig <post@michaelwittig.info>
2012-11-11 13:26:40 +0100

    redid is now local

--- config.js
+++ config.js
@@ -23,9 +23,8 @@ define(["common/logger"], function(logger) {
 	} else {
 		logger.debug("lazy configuration");
 		conf["mongo.url"] = "mongodb://nodejitsu_nko3-stuttgart-js:mn0tol2en4l6k3mhh78mpomsq1@ds039267.mongolab.com:39267/nodejitsu_nko3-stuttgart-js_nodejitsudb1204382411";
-		conf["redis.host"] = "nodejitsudb3383330325.redis.irstack.com";
+		conf["redis.host"] = "localhost";
 		conf["redis.port"] = 6379;
-		conf["redis.passwd"] = "nodejitsudb3383330325.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4";
 		conf["webServer.port"] = 3000;
 		conf["fileServer.dir"] = "./static";
     }

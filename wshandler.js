6332e4b74a8ede8418c85a885be43abdcb77d43f
Michael Wittig <post@michaelwittig.info>
2012-11-11 13:20:25 +0100

    join rooms

--- wshandler.js
+++ wshandler.js
@@ -1,7 +1,7 @@
 /**
  * If a new WebSocket is created it is handled here.
  */
-define(["common/logger", "jsonrpchandler"], function(logger, jsonrpchandler) {
+define(["common/logger", "jsonrpchandler", "common/locroom"], function(logger, jsonrpchandler, locroom) {
 	"use strict";
 
 	return {
@@ -9,7 +9,49 @@ define(["common/logger", "jsonrpchandler"], function(logger, jsonrpchandler) {
 			logger.debug("client connected");
 			websocket.on("jsonrpc", function(jsonrpc, callback) {
 				logger.debug("jsonrpc received", jsonrpc);
-				jsonrpchandler.handle(jsonrpc, callback);
+				if (jsonrpc.method === "board:getall") {
+					logger.debug("board:getall magic");
+					websocket.get("joined:locs", function (err, roomsToLeave) {
+						logger.debug("board:getall magic, check");
+						if (err) {
+							callback(err);
+						} else {
+							if (roomsToLeave) {
+								logger.debug("leave loc rooms " + roomsToLeave);
+								roomsToLeave.forEach(function(roomToLeave) {
+									websocket.leave(roomToLeave);
+								});
+							}
+							var roomsToJoin = locroom.getRooms(jsonrpc.params[0], jsonrpc.params[1]);
+							logger.debug("join loc rooms " + roomsToJoin);
+							websocket.set("joined:locs", roomsToJoin, function () {
+								roomsToJoin.forEach(function(roomToJoin) {
+									websocket.join(roomToJoin);
+								});
+							});
+							jsonrpchandler.handle(jsonrpc, callback);
+						}
+					});
+				} else if (jsonrpc.method === "message:getall") {
+					websocket.get("joined:board", function (err, roomToLeave) {
+						if (err) {
+							callback(err);
+						} else {
+							if (roomToLeave) {
+								logger.debug("leave board room " + roomToLeave);
+								websocket.leave(roomToLeave);
+							}
+							var roomToJoin = "board:" + jsonrpc.params[0];
+							logger.debug("join board room " + roomToJoin);
+							websocket.set("joined:board", roomToJoin, function () {
+								websocket.join(roomToJoin);
+							});
+							jsonrpchandler.handle(jsonrpc, callback);
+						}
+					});
+				} else {
+					jsonrpchandler.handle(jsonrpc, callback);
+				}
 			});
 			websocket.on("disconnect", function() {
 				logger.debug("client disconnected");

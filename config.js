2d8bbc88b02241e0feb86af5b15caf25fa91c571
Michael Wittig <post@michaelwittig.info>
2012-11-11 10:08:48 +0100

    ups

--- config.js
+++ config.js
@@ -13,11 +13,19 @@ define(["common/logger"], function(logger) {
 		conf["redis.passwd"] = "nodejitsudb3383330325.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4";
 		conf["webServer.port"] = 3000;
 		conf["fileServer.dir"] = "./static";
-    } else {
-		logger.debug("developper configuration");
+    } else if (process.env.NODE_ENV === "development") {
+		logger.debug("production configuration");
 		conf["mongo.url"] = "mongodb://localhost/test";
 		conf["redis.host"] = "localhost";
 		conf["redis.port"] = 6379;
+		conf["webServer.port"] = 3000;
+		conf["fileServer.dir"] = "./static";
+	} else {
+		logger.debug("lazy configuration");
+		conf["mongo.url"] = "mongodb://nodejitsu_nko3-stuttgart-js:mn0tol2en4l6k3mhh78mpomsq1@ds039267.mongolab.com:39267/nodejitsu_nko3-stuttgart-js_nodejitsudb1204382411";
+		conf["redis.host"] = "nodejitsudb3383330325.redis.irstack.com";
+		conf["redis.port"] = 6379;
+		conf["redis.passwd"] = "nodejitsudb3383330325.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4";
 		conf["webServer.port"] = 3000;
 		conf["fileServer.dir"] = "./static";
     }

c4acc8138771b2a290fa79fdaefdfabe891f0802
Michael Wittig <post@michaelwittig.info>
2012-11-11 23:02:30 +0100

    fixed demo

--- index.js
+++ index.js
@@ -139,7 +139,6 @@ requirejs(["server", "pubsub", "cache", "common/logger", "jsonrpchandler"], func
 							process.exit(1);
 						} else {
 							logger.notice("started");
-							startDemo(9.157273, 48.771309);
 						}
 					});
 				}

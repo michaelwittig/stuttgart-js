ad086768bdda41bc6927d1465a7387d370123681
Jorin Vogel <jorin.vogel@gmail.com>
2012-11-11 01:39:45 +0100

    rebuilt containerView as manager for the subviews

--- static/js/views/containerView.js
+++ static/js/views/containerView.js
@@ -1,26 +1,54 @@
-define(['backbone', 'utils/registry', 'views/mapView', 'views/boardsListView'], function(Backbone, registry, MapView, BoardsListView) {
+define(['backbone', 'utils/registry', 'views/boardsListView', 'views/boardView'], function(Backbone, registry, BoardsListView , BoardView) {
 
     var ContainerView = Backbone.View.extend({
 
-	el: '#container',
+        el: '#container',
 
-	initialize: function() {
-	    this.mapView = new MapView();
-	    this.boardsListView = new BoardsListView();
+        initialize: function() {
+            this.isHidden = true;
+            this.boardsListView = new BoardsListView();
+            this.boardView = new BoardView();
 
-	    registry.state.on('route:home', function() {
-		this.mapView.show();
-		// this.boardsListView.hide();
-	      //this.boardView.hide();
-	    }, this);
+            registry.state.on('route:home', this.hide, this);
 
-	    registry.state.on('route:boards', function() {
-		this.boardsListView.show();
-		// this.mapView.hide();
-	      //this.boardView.hide();
-	    }, this);
+            registry.state.on('route:boards', function() {
+                this.displayView('boardsListView');
+            }, this);
 
-	}
+            registry.state.on('route:boards', function() {
+                this.displayView('boardView');
+            }, this);
+        },
+
+        displayView: function(view) {
+            if (this.isHidden) {
+                this.open(view);
+                this.show();
+            } else {
+                this.switchTo(view);
+            }
+        },
+
+        show: function() {
+            //TODO: slide in from the bottom
+            this.$el.show();
+            this.isHidden = false;
+        },
+
+        hide: function() {
+            //TODO: slide out to the bottom
+            this.$el.hide();
+            this.isHidden = true;
+        },
+
+        open: function(view) {
+            //TODO: immediately show :view
+        },
+
+        switchTo: function(view) {
+            //TODO: animated switching between the two views
+            //      move container left or right
+        }
     });
 
     return ContainerView;

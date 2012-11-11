72a9ff0c56bb3455108e34c31c9b740085f9ba50
Michael Wittig <post@michaelwittig.info>
2012-11-11 12:15:33 +0100

    closes #14

--- jsonrpchandler.js
+++ jsonrpchandler.js
@@ -1,18 +1,22 @@
 /**
  * JSON-RPC calls are handled here.
  */
-define(["methodhandler"], function(methodhandler) {
+define(["methodhandler", "common/logger"], function(methodhandler, logger) {
 	"use strict";
 
 	return {
 		handle: function(jsonrpc, callback) {
-			methodhandler.handle(jsonrpc.method, jsonrpc.params, function(err, res) {
-				if (err) {
-					callback(err, {result: null, error: err, id: jsonrpc.id});
-				} else {
-					callback(undefined, {result: res, error: null, id: jsonrpc.id});
-				}
-			});
+			try {
+				methodhandler.handle(jsonrpc.method, jsonrpc.params, function(err, res) {
+					if (err) {
+						callback(err, {result: null, error: err, id: jsonrpc.id});
+					} else {
+						callback(undefined, {result: res, error: null, id: jsonrpc.id});
+					}
+				});
+			} catch (err) {
+				callback(err, {result: null, error: err, id: jsonrpc.id});
+			}
 		}
 	};
 });
